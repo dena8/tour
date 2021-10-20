8a3b063c8d8b9c62c41969ef3cd1c5ac019bae28
nikolay <nikolay.topalov@gmail.com>
2021-10-20 21:27:13 +0300

    auth login effect

--- src/app/+store/auth/effects.ts
+++ src/app/+store/auth/effects.ts
@@ -2,41 +2,43 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { catchError, map, switchMap, takeUntil, tap } from 'rxjs/operators';
-import {ActionTypes, namespace} from './action';
-import {UserService} from '../../core/service/user.service';
-import { IAction,ILoggedAction} from '../model/index';
+import {
+    login, loginSuccess, loginFailed, loginCancel,
+    register, registerSuccess, registerFailed,
+} from './action';
+import { UserService } from '../../core/service/user.service';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 
-
 @Injectable()
-export class AuthEffects{
-        
-    constructor(private action$:Actions, private userService:UserService,private toastr:ToastrService,private router:Router){}
+export class AuthEffects {
 
-    register$ = createEffect(()=> this.action$.pipe(
-        ofType<IAction>(`${namespace} ${ActionTypes.register}`),
-        switchMap((action)=>{         
-             return this.userService.postRegister(action.user).pipe(              
-                tap(()=>this.toastr.success("Successful register")),
-                map((data) => (this.router.navigate(['home']), {type:`${namespace} `+ ActionTypes.registerSuccess,...data})),
-                catchError((err)=>of({ type:`${namespace} `+ ActionTypes.registerFailed, ...err }))
-             )
+    constructor(
+        private action$: Actions,
+        private userService: UserService,
+        private toastr: ToastrService,
+        private router: Router
+    ) { }
+
+    register$ = createEffect(() => this.action$.pipe(
+        ofType(register),
+        switchMap(payload => {
+            return this.userService.postRegister(payload.user).pipe(
+                tap(() => this.toastr.success("Successful register")),
+                map((data) => (this.router.navigate(['home']), { type: registerSuccess, ...data })),
+                catchError((err) => of({ type: registerFailed, ...err }))
+            )
         })
     ));
- 
 
-    login$ = createEffect(()=> this.action$.pipe(
-        ofType<ILoggedAction>(`${namespace} ${ActionTypes.login}`),
-        switchMap((action)=> this.userService.login({...action}).pipe( 
-            tap(user=>console.log("FROM EFFECT ",user)),          
-            tap((user)=>localStorage.setItem('username',action.username)),            
-            tap(()=>this.toastr.success("Successful login")),
-            takeUntil(this.action$.pipe(ofType(ActionTypes.loginCancel))),           
-            map((user)=>(this.router.navigate(['home']),{type:`${namespace} ${ActionTypes.loginSuccess}`, ...user})),
-            catchError((err)=>of({ type:`${namespace} `+ ActionTypes.loginFailed, ...err }))
+    login$ = createEffect(() => this.action$.pipe(
+        ofType(login),
+        switchMap(({ username, password }) => this.userService.login({ username, password }).pipe(
+            takeUntil(this.action$.pipe(ofType(loginCancel))),
+            map(user => (this.router.navigate(['home']), { type: loginSuccess, ...user })),
+            catchError((err) => of({ type: loginFailed, ...err }))
         ))
 
     ))
-  
+
 }
