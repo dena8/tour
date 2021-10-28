3bae9c0f9b09a0b9e293ba7ca4140324bc3b57b8
nikolay <nikolay.topalov@gmail.com>
2021-10-28 21:21:48 +0300

    after dispatching order,listening for success/failure effect before next dispatch

--- src/app/buy/cart/cart.component.ts
+++ src/app/buy/cart/cart.component.ts
@@ -1,15 +1,15 @@
 
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { BuyService } from 'src/app/core/service/buy.service';
-
-import {ITour as ITourBuy,} from '../../+store/model/buy';
+import { Observable, race } from 'rxjs';
+import { } from '@ngrx/effects'
+import { ITour as ITourBuy, } from '../../+store/model/buy';
 import { Store } from '@ngrx/store';
-import {cart} from '../../+store/buy/selector';
-import {removeTour,clearCart} from '../../+store/buy/action';
-import {createOrder} from '../../+store/global/action';
-import {map} from 'rxjs/operators';
+import { cart } from '../../+store/buy/selector';
+import { removeTour, clearCart } from '../../+store/buy/action';
+import { createOrder } from '../../+store/global/action';
+import { filter, map, mapTo, take } from 'rxjs/operators';
+import { GlobalEffects } from 'src/app/+store/global/effects';
+import { createOrderSuccess, createOrderFailed } from 'src/app/+store/global/action';
 
 @Component({
   selector: 'app-cart',
@@ -17,32 +17,38 @@ import {map} from 'rxjs/operators';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent implements OnInit {
-  
+
   tourSum$: Observable<number>;
-  username:string;
-  cart$:Observable<ITourBuy[]>;
+  username: string;
+  cart$: Observable<ITourBuy[]>;
 
 
-  constructor(private buyService:BuyService,private store:Store,  private router: Router){}
-   
+  constructor(private store: Store, private globalEffects: GlobalEffects) { }
+
 
   ngOnInit(): void {
-    this.cart$ = this.store.select(cart.get); 
-    this.tourSum$ = this.store.select(cart.sum);     
+    this.cart$ = this.store.select(cart.get);
+    this.tourSum$ = this.store.select(cart.sum);
   }
 
-  order() {   
-    this.cart$.pipe(map(t=> t.reduce((acc,t)=>{
-     acc.push(t.name);  
-     return acc;   
-    },[]))).subscribe(cartItems=> this.store.dispatch(createOrder({cartItems})));  
-      
-    this.store.dispatch(clearCart());
+  order() {
+    this.cart$.pipe(take(1), map(toursList => toursList.map(t => t.name)))
+      .subscribe(cartItems => {
+        this.store.dispatch(createOrder({ cartItems }));
+        race(
+          this.globalEffects.createOrder$.pipe(filter(action => action.type === createOrderSuccess), mapTo(true)),
+          this.globalEffects.createOrder$.pipe(filter(action => action.type === createOrderFailed), mapTo(false))
+        ).pipe(take(1)).subscribe(result => {
+          if (!result) { return; }
+          this.store.dispatch(clearCart());
+        })
+      });
+
   }
 
   removeItem(name: string) {
-    this.store.dispatch(removeTour({name}));
-    this.ngOnInit;    
+    this.store.dispatch(removeTour({ name }));
+    this.ngOnInit;
   }
- 
+
 }
